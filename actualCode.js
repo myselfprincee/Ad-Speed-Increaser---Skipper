885f728229c7f70944ec01e8773d28655674ce01
Prince Gupta <prince.kumar01129@gmail.com>
2024-04-24 19:53:26 +0530

    Changes in the actualCode

--- actualCode.js
+++ actualCode.js
@@ -1,16 +1,35 @@
-var button = document.getElementsByClassName("ytp-ad-skip-button-modern ytp-button")[0];
-var unskippableOne = document.getElementsByClassName("ytp-ad-preview-container ytp-ad-preview-container-detached");
-var flyoutCTA = document.getElementsByClassName("ytp-ad-text ytp-ad-preview-text-modern")[0];
-
-if (flyoutCTA.innerText = 'Video plays soon') {
-    document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 11.0;
+var button = document.querySelector(".ytp-ad-skip-button-modern");
+if (!button) {
+    button = document.querySelector(".ytp-skip-ad-button");
+}
+if (!button) {
+    button = document.querySelector(".ytp-button");
 }
 
-if (button.length > 0 || unskippableOne.length > 0) {
-    document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = 11.0;
+var unskippableOne = document.querySelector(".ytp-ad-preview-container .ytp-ad-preview-container-detached");
+if (!unskippableOne) {
+    unskippableOne = document.querySelector(".ytp-ad-preview-container-detached");
+}
+
+var flyoutCTA = document.querySelector(".ytp-ad-text .ytp-ad-preview-text-modern");
+if (!flyoutCTA) {
+    flyoutCTA = document.querySelector(".ytp-ad-preview-text-modern");
+}
+
+var VideoElem = document.querySelector(".video-stream")
+if (!VideoElem) {
+    VideoElem = document.querySelector(".html5-main-video");
+}
+
+if (button.innerText.length > 0 || unskippableOne.innerText.length > 0) {
+    VideoElem.playbackRate = 11.0;
     if (button.innerText = "Skip") {
         button.click();
+    } else if (flyoutCTA.innerText = 'Video plays soon') {
+        VideoElem.playbackRate = 11.0;
     }
+
+
     setTimeout(function () {
         chrome.browserAction.onClicked.addListener(function (tab) {
             chrome.tabs.executeScript(
